94b55c005eca608b565e2a08fcf124fe75c2e5c2
USER <USER@192.168.0.16>
2017-11-05 19:53:41 +0100

    test

---
+++ src/filrouge/File.java
@@ -0,0 +1,12 @@
+package filrouge;
+
+import java.util.LinkedList;
+
+public interface File<T> {
+	
+	public LinkedList<T> getElements();
+	
+	default void push(T x){
+		this.getElements().add(x);
+	}
+}
