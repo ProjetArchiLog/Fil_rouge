87de7db86224523cfaa7832b8079b44d2abe1e1e
denis <denis@PC-Denis>
2017-11-08 17:54:21 +0100

    IterateurListe complété

--- src/tp/v2/IterateurListe.java
+++ src/tp/v2/IterateurListe.java
@@ -1,19 +1,31 @@
 package tp.v2;
 
 import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 public class IterateurListe<E> implements Iterator<E> {
 
+	private int current;
+	private List<E> myList;
+	
+	public IterateurListe(List<E> l) {
+		this.myList = l;
+		current = 0;
+	}
 	@Override
 	public boolean hasNext() {
-		// TODO Auto-generated method stub
-		return false;
+		return !(myList.size() == current);
 	}
 
 	@Override
 	public E next() {
-		// TODO Auto-generated method stub
-		return null;
+		if (this.hasNext()) {
+			return (myList.get(current++));
+		}
+		else {
+			throw new NoSuchElementException("There are no elements size = " + myList.size());
+        }	
 	}
 
 }
