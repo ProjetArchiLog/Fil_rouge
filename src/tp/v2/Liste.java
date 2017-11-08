09a18521eb0a67a4d920d7ef9b905d6e9316701b
USER <USER@ZENBOOK>
2017-11-08 20:29:37 +0100

    commentaires lire

--- src/tp/v2/Liste.java
+++ src/tp/v2/Liste.java
@@ -5,21 +5,28 @@ public interface Liste<E> extends Iterable<E> {
 	/*
 	 * Accesseurs
 	 */
+	
+	// Indique si la liste est de type vide
 	default boolean casVide() {
 		return false;
 	}
+	// Renvoie la tete de la liste
 	default E tete() {
 		throw new UnsupportedOperationException();
 	}
+	// Renvoie la liste sans la tete
 	default Liste<E> reste() {
 		throw new UnsupportedOperationException();
 	}
+	// Indique si la liste possib
 	default boolean casCons() {
 		return false;
 	}
+	// Indique si la liste est de type cons
 	default public int taille(){
 		return 0;
 	}
+	// Indique si la liste est vide
 	default public boolean estVide(){
 		return this.taille() == 0;
 	}
@@ -27,9 +34,11 @@ public interface Liste<E> extends Iterable<E> {
 	/*
 	 * Services
 	 */
+	// Renvoie un iterateur de la liste
 	default Iterator<E> iterator() {
 		return new IterateurListe<E>(this); // Compléter puis utiliser IterateurListe.
 	}
+	// Renvoie la liste dans l'ordre inverse
 	default Liste<E> miroir(){
 		Liste<E> newListe=vide();
 		Iterator<E> iterateur=iterator();
@@ -42,6 +51,7 @@ public interface Liste<E> extends Iterable<E> {
 	 * Fabriques (statiques)
 	 */
 	
+	// Construit une liste vide
 	public static <E> Liste<E> vide() {
 		return new Liste<E>() {
 			public boolean casVide(){
@@ -50,6 +60,7 @@ public interface Liste<E> extends Iterable<E> {
 		};
 	}
 	
+	// Construit une liste en rajoutant l'element t a une liste r
 	public static <E> Liste<E> cons(E t, Liste<E> r) {
 		return new Liste<E>() {
 			public E tete(){
